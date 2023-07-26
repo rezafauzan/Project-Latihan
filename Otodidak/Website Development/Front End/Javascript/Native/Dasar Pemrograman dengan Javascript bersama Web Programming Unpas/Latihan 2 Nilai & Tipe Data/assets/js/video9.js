68fdfcc9e49bf6eb60fcd80fd474476dc3321477
rezafauzan <rezafauzan593@gmail.com>
2023-07-26 16:55:53 +0700

    Selesai Video 8,9 tentang Bagaimana nilai dan type data bekerja pada komputer,tipe data yang ada di dalam javascript, jenis-jenis angka pada tipe data number di javascript(tanpa desimal,dengan desimal,dengan desimal,Eksponen,Negatif,Oktal,Hexadesimal,Special Number,Infinity & -Infinity,NaN) dan Operasi Matematika akan dijalankan juga jika sebuah angka dioperasikan pada string angka

--- Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Latihan 2 Nilai & Tipe Data/assets/js/video9.js
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Latihan 2 Nilai & Tipe Data/assets/js/video9.js
@@ -1,43 +1,68 @@
 // Angka Dalam Javascript
+console.log('Angka Dalam Javascript');
 
 // tanpa desimal
+console.log('tanpa desimal');
 let angka = 10
 console.log(angka);
+console.log('\n')
 
 // dengan desimal
+console.log('dengan desimal');
 angka = 0.2
 console.log(angka);
+console.log('\n')
 
 // dengan desimal
+console.log('dengan desimal');
 angka = 0.2
 console.log(angka);
+console.log('\n')
 
 // Eksponen
+console.log('eksponen');
 angka = 123e5
 let angka2 = 123e-5
 console.log(angka);
 console.log(angka2);
+console.log('\n')
 
 // Negatif
+console.log('negatif');
 angka = -2
 console.log(angka);
+console.log('\n')
 
 // Oktal
+console.log('oktal');
 angka = 022
 console.log(angka);
+console.log('\n')
 
-// HexaDesimal
+// Hexadesimal
+console.log('hexa desimal');
 angka = 0xFF
 console.log(angka);
+console.log('\n')
 
 // Special Number
+console.log('Special Number');
+
 // Infinity & -Infinity
+console.log('Infinity & -Infinity');
 angka = Infinity // 2/0
 angka2 = -Infinity // -2/0
 console.log(angka);
 console.log(angka2);
+console.log('\n')
 
-// Special Number
 // NaN
-angka = NaN // 2/0
+console.log('NaN');
+angka = NaN // 2/"String"
+console.log(angka);
+console.log('\n')
+
+// Operasi Matematika akan dijalankan juga jika sebuah angka dioperasikan pada string angka
+console.log('Operasi Matematika akan dijalankan juga jika sebuah angka dioperasikan pada string angka');
+angka = 100/"10"
 console.log(angka);
