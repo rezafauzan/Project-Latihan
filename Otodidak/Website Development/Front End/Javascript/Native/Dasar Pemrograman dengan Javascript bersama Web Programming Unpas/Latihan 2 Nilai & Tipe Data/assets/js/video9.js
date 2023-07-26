5533ae065f25ce23070088d67bc3c4fea41dba96
rezafauzan <rezafauzan593@gmail.com>
2023-07-26 16:45:30 +0700

    Selesai Belajar Sampai Video 7 tentang Dasar Pemrograman dengan javascript,Apa itu Pemrograman?,Bahasa Pemrograman,Compiler & Interpreter,Kenapa Javascript?,Sejarah Javascript,dan Lingkungan & Penulisan Source Code Javascript

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Latihan 2 Nilai & Tipe Data/assets/js/video9.js
@@ -0,0 +1,43 @@
+// Angka Dalam Javascript
+
+// tanpa desimal
+let angka = 10
+console.log(angka);
+
+// dengan desimal
+angka = 0.2
+console.log(angka);
+
+// dengan desimal
+angka = 0.2
+console.log(angka);
+
+// Eksponen
+angka = 123e5
+let angka2 = 123e-5
+console.log(angka);
+console.log(angka2);
+
+// Negatif
+angka = -2
+console.log(angka);
+
+// Oktal
+angka = 022
+console.log(angka);
+
+// HexaDesimal
+angka = 0xFF
+console.log(angka);
+
+// Special Number
+// Infinity & -Infinity
+angka = Infinity // 2/0
+angka2 = -Infinity // -2/0
+console.log(angka);
+console.log(angka2);
+
+// Special Number
+// NaN
+angka = NaN // 2/0
+console.log(angka);
