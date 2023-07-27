93a245424b8a1d1f71ff9a3e37944e044f850a52
rezafauzan <rezafauzan593@gmail.com>
2023-07-27 17:49:20 +0700

    Selesai Video 12 Tentang String

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Latihan 4 String/assets/js/script.js
@@ -0,0 +1,9 @@
+// String
+console.log("String")
+console.log("ini adalah string")
+console.log("string diapit oleh kutip bisa kutip satu atau kutip dua")
+console.log("string yang memiliki kutip didalamnya harus dibungkus kedalam kutip yang tepat, lihat catatan.docx untuk lebih lengkapnya")
+console.log("atau menggunakan escape character \'\"\` dll")
+console.log("dua string atau lebih juga bisa dibandingkan dan digabungkan(menggunakan operator concatenated)")
+console.log("dan untuk membandingkan dua buah string harus hati hati karena sensitive case")
+console.log("string juga memiliki built in function seperti .length untuk menghitung jumlah character(termasuk blank space) pada string")
