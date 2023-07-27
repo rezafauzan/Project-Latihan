e614928c34f666a66ea40434edd9f8f484f02b7a
rezafauzan <rezafauzan593@gmail.com>
2023-07-27 18:15:34 +0700

    Selesai Video 13 Tentang Boolean, Boolean(), nilai Truthy | Falsy, Undefined dan NULL

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Latihan 5 Boolean/assets/js/script.js
@@ -0,0 +1,18 @@
+// Boolean
+console.log("Boolean")
+console.log("ekspresi \"(10>2)\" menghasilkan nilai boolean" + (10>2))
+console.log("ekspresi \"(10<2)\" menghasilkan nilai boolean" + (10<2))
+console.log("fungsi Boolean((10<2)) menghasilkan nilai boolean" + Boolean((10<2)))
+console.log("fungsi Boolean(10) menghasilkan nilai boolean" + Boolean((10)))
+console.log("fungsi Boolean(\"Reza\") menghasilkan nilai boolean" + Boolean(("Reza")))
+console.log("fungsi Boolean(0) menghasilkan nilai boolean" + Boolean((0)))
+console.log("fungsi Boolean() satu nilai akan menghasilkan true or false karena sejatinya nilai di javascript mempunyai nilai truthy or false lihat lebih lengkap di catatan")
+
+// Undifined
+let x
+console.log('nilai x =' + x + " karena variable x dideklarasikan tapi belum memiliki nilai")
+
+// NULL
+
+let s = null
+console.log('nilai s =' + s + " yaitu nilai kosong yang bisa dimasukan pada variable")
