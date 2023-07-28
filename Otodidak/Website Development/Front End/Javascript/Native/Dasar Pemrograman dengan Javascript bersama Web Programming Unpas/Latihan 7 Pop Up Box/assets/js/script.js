8d9148970c104f5e8852e8467fce63e316706aee
rezafauzan <rezafauzan593@gmail.com>
2023-07-28 16:09:29 +0700

    Selesai Video 16 tentang Popup Box alert,prompt,confirm

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Latihan 7 Pop Up Box/assets/js/script.js
@@ -0,0 +1,22 @@
+// Pop Up Box
+
+// Alert
+alert("Ini hasil dari Pop Up Box Alert")
+alert("Ini hasil dari Pop Up Box Alert kedua kali nya xixi XD")
+
+// Prompt
+Prompt("Ini hasil dari Pop Up Box Prompt")
+let x = Prompt("Namamu Siapa ?")
+
+alert("Selamat Datang " + x)
+
+// Confirm
+confirm("Ini hasil dari Pop Up Box confirm")
+
+let konfirmasi = confirm("Yakin?")
+
+if (konfirmasi == true) {
+    Alert("user yakin")
+} else {
+    Alert("user tidak yakin")
+}
