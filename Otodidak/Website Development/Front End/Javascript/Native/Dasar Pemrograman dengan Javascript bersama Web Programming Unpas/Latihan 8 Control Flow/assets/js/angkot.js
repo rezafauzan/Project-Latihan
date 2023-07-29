b197fbdc050bb78b4807852702ecef40453a55b9
rezafauzan <rezafauzan593@gmail.com>
2023-07-29 23:22:00 +0700

    Selesai video 17,18 Tentang Control Flow : Pengulangan While,Latihan Pengulangan Menampilkan Status Operasi Angkot dan Tabel Penelusuran

---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Latihan 8 Control Flow/assets/js/angkot.js
@@ -0,0 +1,7 @@
+let i = 1
+while( i <= 10 ){
+
+    console.log( 'Angkot No. ' + i + ' beroperasi dengan baik.' )
+    i++
+
+}
---
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Latihan 8 Control Flow/assets/js/script.js
@@ -0,0 +1,12 @@
+// Control Flow
+    // Pengulangan
+    console.log("Pengulangan")
+        // While Loop
+            // while (true) {
+            //     console.log("Halo Ini Hasil While Loop" +" ini akan menghasilkan infinity loop,lihat catatan.docx")
+            // }
+            let kondisi = 0
+            while (kondisi<10) {
+                console.log("Ini Adalah Iterasi ke - " + kondisi)
+                kondisi++
+            }
