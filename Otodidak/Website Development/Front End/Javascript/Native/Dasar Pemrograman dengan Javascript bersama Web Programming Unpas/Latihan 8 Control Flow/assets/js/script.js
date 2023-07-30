118e3af50ee65381a3c29901ed5c81b234e1c779
rezafauzan <rezafauzan593@gmail.com>
2023-07-30 23:48:42 +0700

    Selesai Video 21,22 Tentang If Else,Mengoptimalkan Program Kelola Angkot menggunakan 1 pengulangan menggunakan for dan if else, latihan ganjil genap, else if dan menambahkan angkot no 8 sedang lembur pada program kelola angkot latihan ke 4

--- Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Latihan 8 Control Flow/assets/js/script.js
+++ Otodidak/Website Development/Front End/Javascript/Native/Dasar Pemrograman dengan Javascript bersama Web Programming Unpas/Latihan 8 Control Flow/assets/js/script.js
@@ -14,3 +14,22 @@
             for (let nilaiAwal = 1 ; nilaiAwal < 10 ; nilaiAwal++){
                 console.log("Ini Adalah Iterasi ke - " + nilaiAwal + " Dari Pengulangan For")
             }
+    // Percabangan / Pengkondisian
+        // If Else
+            let angka = prompt("Masukan Angka")
+            if ( angka%2 == 0 ) {
+                alert("Angka " + angka + "Merupakan Bilangan Genap dan ini merupakan hasil dari if")
+            } else {
+                alert("Angka " + angka + "Merupakan Bilangan Ganjil dan ini merupakan hasil dari else")
+            }
+        // Else If
+            angka = prompt("Masukan Angka")
+            if ( angka%2 == 0 ) {
+                alert("Angka " + angka + "Merupakan Bilangan Genap dan ini merupakan hasil dari if")
+            } else if ( angka%2 != 0 ) {
+                alert("Angka " + angka + "Merupakan Bilangan Ganjil dan ini merupakan hasil dari else if")
+            }
+            else {
+                alert(angka + " Bukan Merupakan Sebuah Angka dan ini merupakan hasil dari else")
+            }
+
